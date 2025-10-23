2e8be326f851df9200044f09ffce345b8b1d571f
femiclassic11-ai <femiclassic11@gmail.com>
2025-10-23 12:30:16 +0100

    update2

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 ///to store grocery items
 
-const groceryItems = [
-    {name: "rice", price: 1500},
-    {name: "beans", price: 1500},
-    {name: "plantain", price: 2000},
-    {name: "spaghetti", price: 1000},
-    {name: "pasta", price: 1500},
-    {name: "semo", price: 7500},
-    {name: "garri", price: 2500},
-    
-];
+const groceryItems = [];
 
 ///inputs and button id's
 const itemInput = document.getElementById("itemInput");
